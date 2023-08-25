6e894922f8507b5acf8ad0fe6b4bec25f44ad5e9
joecjw <joechan1520@gmail.com>
2023-08-25 18:07:37 +0800

    Fix JwtPractice Cross Origin Error

--- JwtPractice/src/main/java/com/joecjw/JwtPractice/controller/TestController.java
+++ JwtPractice/src/main/java/com/joecjw/JwtPractice/controller/TestController.java
@@ -1,11 +1,9 @@
 package com.joecjw.JwtPractice.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
